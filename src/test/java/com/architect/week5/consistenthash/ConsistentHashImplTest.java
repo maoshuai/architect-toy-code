62c8a3d4f1b2fc150cdd62ca0c253e15aed6f6b4
maoshuai <zhucemid@gmail.com>
2020-10-28 16:22:36 +0800

    add test: getServingNode_shouldAlwaysReturnNode1_givenOneNode

--- src/test/java/com/architect/week5/consistenthash/ConsistentHashImplTest.java
+++ src/test/java/com/architect/week5/consistenthash/ConsistentHashImplTest.java
@@ -7,14 +7,20 @@ import java.util.TreeMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ConsistentHashImplTest {
-    private ConsistentHashImpl consistentHash = new ConsistentHashImpl(150, new HashFunctionImpl());
-
     @Test
-    void addNode_shouldContains150Nodes_whenAddOneNode(){
+    void addNode_shouldContains150Nodes_whenAddTheFirstNode(){
+        ConsistentHashImpl consistentHash = new ConsistentHashImpl(150, new HashFunctionImpl());
         consistentHash.addNode("node1");
         TreeMap<Integer, String> nodes = consistentHash.getNodes();
         assertThat(nodes.size()).isEqualTo(150);
     }
 
+    @Test
+    void getServingNode_shouldAlwaysReturnNode1_givenOneNode(){
+        ConsistentHashImpl consistentHash = new ConsistentHashImpl(150, new HashFunctionImpl());
+        consistentHash.addNode("node1");
+        String servingNode = consistentHash.getServingNode("key");
+        assertThat(servingNode).isEqualTo("node1");
+    }
 
 }
