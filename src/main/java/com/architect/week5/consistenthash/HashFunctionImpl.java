475d976287b5d86b609a673898c8ef788501972f
maoshuai <zhucemid@gmail.com>
2020-10-28 19:45:30 +0800

    using guava crc32 as hasing function

--- src/main/java/com/architect/week5/consistenthash/HashFunctionImpl.java
+++ src/main/java/com/architect/week5/consistenthash/HashFunctionImpl.java
@@ -1,10 +1,18 @@
 package com.architect.week5.consistenthash;
 
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.Charset;
 import java.util.Objects;
 
 public class HashFunctionImpl implements HashFunction {
+    com.google.common.hash.HashFunction googleHashFunc = Hashing.crc32();
     @Override
     public int hash(String object) {
-        return Objects.hash(object);
+
+        Hasher hasher = googleHashFunc.newHasher();
+        hasher.putString(object, Charset.defaultCharset());
+        return hasher.hash().asInt();
     }
 }
